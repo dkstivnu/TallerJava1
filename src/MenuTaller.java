e7d5a08c67bc76450c8e14a493cf1a3b39f3e053
stivn <wilstivenortega@gmail.com>
2025-03-01 14:30:41 -0500

    Menu añadido

---
+++ src/MenuTaller.java
@@ -0,0 +1,21 @@
+public class MenuTaller {
+
+    public static void main(String[] args) {
+
+        printMenu();
+
+
+
+    }
+
+    private static void printMenu() {
+        System.out.println("--------- MENU TALLER ----------");
+        System.out.println("1. Calcular el area de un triangulo");
+        System.out.println("2. Precio de las fotocopias");
+        System.out.println("3. Mostrar la tabla de multiplicar");
+        System.out.println("4. Encontrar cociente y residuo");
+        System.out.println("5. Salir");
+        System.out.println("--------- MENU TALLER ----------");
+    }
+
+}
